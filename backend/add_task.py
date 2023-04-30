457ad87de026f62692adafa54e8dcd13ecb62422
chong <solesschong@gmail.com>
2023-04-30 12:20:47 +0800

    Refactor add_task. Take cmd args.

--- backend/add_task.py
+++ backend/add_task.py
@@ -86,23 +86,76 @@ def LEGACY():
             db.session.commit()
             celery_worker.task_render_scene.delay(task.id)
 
-
+def render_person_on_scenes(person_id, scene_list):
+    task_id_list = []
+    for scene in scene_list:
+        task = Task(scene_id=scene.scene_id, person_id_list = [person_id])
+        db.session.add(task)
+        db.session.commit()
+        task_id_list.append(task.id)
+    ch = chain(
+        group([signature('set_up_scene', (scene.scene_id,)) for scene in scene_list]),
+        group([signature('render_scene', (task_id,), immutable=True) for task_id in task_id_list])
+    )
+    ch.apply_async()
 
 # main script
 if __name__ == "__main__":
+    # Arg parser for arg 'cmd', options: ['render_all_wait', 'collection_prefix', 'collection_name', 'scene_id']
+    # Arg parser for arg 'name', '-n' for short, string.
+    # Arg parser for arg 'person_list', '-p', type=list, required=True, help='Person id list.'
+    # when cmd is ['collection_prefix', 'collection_name'], arg 'name' is required.
+    import argparse
+    parser = argparse.ArgumentParser(description='Add task to Celery srender queue.')
+    parser.add_argument('cmd', type=str, choices=['render_all_wait', 'collection_prefix', 'collection_name'], help='Command to execute.')
+    parser.add_argument('-n', '--name', type=str, help='Name of collection or prefix of collection name.')
+    parser.add_argument('-p', '--person_list', type=int, nargs='+', required=True, help='Person id list.')
 
-    ### Collection name X user -> task
-    collection_name_prefix_list = [
-        u'古风摄影师作品合集\\七奈Nanako\\2021\\夏日炎炎下的可爱子', 
-        u'古风摄影师作品合集\\七奈Nanako\\2021\\响当当的大铭', 
-        u'古风摄影师作品合集\\七奈Nanako\\2021\\云知笑 3', 
-        u'古风摄影师作品合集\\七奈Nanako\\2021\\云知笑 @王楚Dino',
-        u'古风摄影师作品合集\\七奈Nanako\\2021\\Shirley-Dan花祀',
-        u'古风摄影师作品合集\\七奈Nanako\\2021\\云知笑',
-        u'古风摄影师作品合集\\七奈Nanako\\2021\\lulusmile露露斯麦尔',
-        u'古风摄影师作品合集\\七奈Nanako\\2021\\lolita',
-        u'古风摄影师作品合集\\七奈Nanako\\2021\\Elpress',
-    ]
+    args = parser.parse_args()
+    logging.info(f'args: {args}')
+
+    # exit if no cmd
+    if not args.cmd:
+        logging.error('No cmd specified.')
+        exit()
+
+    cmd = args.cmd
+
+    if cmd == 'render_all_wait':
+        # celery task for all 'wait' Task in db
+        task_list = Task.query.filter(Task.status=='wait').all()
+        task_id_list = [task.id for task in task_list]
+        print(len(task_id_list))
+        for id in task_id_list:
+            celery.send_task('render_scene', (id,), immutable=True)
+    elif cmd == 'collection_prefix':
+        collection_prefix = args.name
+        logging.info(f'collection_name_prefix: {collection_prefix}')
+        scene_list = Scene.query.filter(Scene.collection_name.startswith(collection_prefix.replace('\\', '\\\\'))).all()
+        for person in args.person_list:
+            render_person_on_scenes(person, scene_list)
+    elif cmd == 'collection_name':
+        collection_name = args.name
+        logging.info(f'collection_name: {collection_name}')
+        scene_list = Scene.query.filter(Scene.collection_name==collection_name).all()
+        logging.info(f'Found {len(scene_list)} scenes.')
+        for person in args.person_list:
+            render_person_on_scenes(person, scene_list)
+
+    # ### Collection name X user -> task
+    # collection_name_prefix_list = [
+    #     u'古风摄影师作品合集\\七奈Nanako\\2021\\夏日炎炎下的可爱子', 
+    #     u'古风摄影师作品合集\\七奈Nanako\\2021\\响当当的大铭', 
+    #     u'古风摄影师作品合集\\七奈Nanako\\2021\\云知笑 3', 
+    #     u'古风摄影师作品合集\\七奈Nanako\\2021\\云知笑 @王楚Dino',
+    #     u'古风摄影师作品合集\\七奈Nanako\\2021\\Shirley-Dan花祀',
+    #     u'古风摄影师作品合集\\七奈Nanako\\2021\\云知笑',
+    #     u'古风摄影师作品合集\\七奈Nanako\\2021\\lulusmile露露斯麦尔',
+    #     u'古风摄影师作品合集\\七奈Nanako\\2021\\lolita',
+    #     u'古风摄影师作品合集\\七奈Nanako\\2021\\Elpress',
+    #     u'古风摄影师作品合集\\七奈Nanako\\2021\\#恋上冬日#',
+    #     u'古风摄影师作品合集\\七奈Nanako\\2020\\风霖',
+    # ]
     # collection_name_prefix_list = [
     #     u'古风摄影师作品合集\\七奈Nanako\\2017\\#好想看你穿制服的样子#',
     #     u'古风摄影师作品合集\\七奈Nanako\\2017\\#花儿和少年#',
@@ -155,10 +208,11 @@ if __name__ == "__main__":
     #     ch.apply_async()
 
 
-    for collection_name_prefix in collection_name_prefix_list:
-        scene_list = Scene.query.filter(Scene.collection_name.startswith(collection_name_prefix.replace('\\', '\\\\'))).all()
-        person_id = 8
-        for scene in scene_list:
-            task = Task(scene_id=scene.scene_id, person_id_list =[person_id], status='wait')
-            db.session.add(task)
-        db.session.commit()
+    # for collection_name_prefix in collection_name_prefix_list:
+    #     scene_list = Scene.query.filter(Scene.collection_name.startswith(collection_name_prefix.replace('\\', '\\\\'))).all()
+    #     person_id = 17
+    #     for scene in scene_list:
+    #         task = Task(scene_id=scene.scene_id, person_id_list =[person_id], status='wait')
+    #         db.session.add(task)
+    #     db.session.commit()
+
