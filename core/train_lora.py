3a6c92248a5742ece54a32ef7e7dbe0cd32c2dc5
chong <solesschong@gmail.com>
2023-05-07 11:07:06 +0800

    Add augmentation.

--- core/train_lora.py
+++ core/train_lora.py
@@ -169,11 +169,12 @@ def train_lora(dataset_path, subject_name, class_name, epoch=5):
         --text_encoder_lr=5e-5 --unet_lr=0.0001 --network_dim=256 \
         --output_name="{subject_name}" \
         --lr_scheduler_num_cycles="10" --learning_rate="0.001" --lr_scheduler="cosine" \
-        --lr_warmup_steps="100" --train_batch_size="12" \
+        --lr_warmup_steps="100" --train_batch_size="4" \
         --max_train_epochs="{epoch}" \
         --save_every_n_epochs="1" --mixed_precision="fp16" --save_precision="fp16" \
         --optimizer_type="AdamW8bit" --max_data_loader_n_workers="0" --bucket_reso_steps=64 \
         --xformers --bucket_no_upscale --random_crop \
+        {"--color_aug --flip_aug" if img_count < 20 else ""} \
         > {Path(dataset_path) / "train.log"} 2>&1
         """
         # --max_train_steps="2"
@@ -200,7 +201,7 @@ def train_lora(dataset_path, subject_name, class_name, epoch=5):
 def main():
     args = parser.parse_args()
     ## Flags
-    remove_bg = True
+    remove_bg = False
     enlarge_face = 2
     ## 1. Prepare dataset
     #
