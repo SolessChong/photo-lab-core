ad2f469724f8c9ed25dcf6ebe51d02cfdbbc63be
Yi <zlfengyi@gmail.com>
2023-05-08 04:10:59 +0800

    1）修改fake_app展示用户结果 2）为lora训练增加抠图后的图片

--- backend/aliyun_face_detector.py
+++ backend/aliyun_face_detector.py
@@ -1,9 +1,13 @@
+import base64
 import io
+import logging
 from PIL import Image
 from alibabacloud_facebody20191230.client import Client
 from alibabacloud_facebody20191230.models import DetectFaceAdvanceRequest
 from alibabacloud_tea_openapi.models import Config
 from alibabacloud_tea_util.models import RuntimeOptions
+from alibabacloud_imageseg20191230.client import Client as ImageSegClient
+from alibabacloud_imageseg20191230.models import SegmentBodyAdvanceRequest
 
 # 阿里云API密钥
 access_key_id = 'LTAINBTpPolLKWoX'
@@ -13,6 +17,13 @@ config = Config(
     access_key_id=access_key_id,
     access_key_secret=access_key_secret,
     endpoint='facebody.cn-shanghai.aliyuncs.com',
+    region_id='cn-shanghai'
+)
+
+image_seg_config = Config(
+    access_key_id=access_key_id,
+    access_key_secret=access_key_secret,
+    endpoint='imageseg.cn-shanghai.aliyuncs.com',
     region_id='cn-shanghai'
 )
 
@@ -135,3 +146,53 @@ def crop_16_9_pil(image_data, face_coordinates):
     cropped_image_bytes = cropped_image_bytes.getvalue()
 
     return cropped_image_bytes
+
+def aliyun_human_segmentation(img: bytes) -> bytes:
+    # 获取人脸坐标
+    face_coordinates = get_face_coordinates(img)
+
+    # 将人脸坐标矩阵放大到原来的2倍
+    x, y, width, height = face_coordinates
+    new_width, new_height = width * 2, height * 2
+    left, top = x - (new_width - width) // 2, y - (new_height - height) // 2
+    right, bottom = left + new_width, top + new_height
+
+    # 处理边界情况
+    img_pil = Image.open(io.BytesIO(img))
+    left = max(0, left)
+    top = max(0, top)
+    right = min(img_pil.width, right)
+    bottom = min(img_pil.height, bottom)
+
+    # 截取放大后的人脸区域
+    cropped_image = img_pil.crop((left, top, right, bottom))
+
+    # 将截取后的图像转换为字节对象
+    cropped_image_bytes = io.BytesIO()
+    cropped_image.save(cropped_image_bytes, format='JPEG')
+
+    # 创建 SegmentBodyRequest 对象
+    segment_body_request = SegmentBodyAdvanceRequest()
+    segment_body_request.image_urlobject = cropped_image_bytes
+
+    runtime = RuntimeOptions()
+
+    try:
+        # 初始化 ImageSegClient
+        image_seg_client = ImageSegClient(image_seg_config)
+        response = image_seg_client.segment_body_advance(segment_body_request, runtime)
+
+        if response is not None and response.data is not None:
+            # 获取响应中的 base64 编码的结果图像
+            result_img_base64 = response.data
+
+            # 将 base64 编码的结果图像转换为字节
+            result_img_bytes = base64.b64decode(result_img_base64)
+            return result_img_bytes
+        else:
+            print("Error: No response or data received.")
+    except Exception as error:
+        print("Error:", error)
+        print("Error code:", error.code)
+
+    return None
---
+++ exps/add_split.py
@@ -0,0 +1,39 @@
+from backend import models
+from backend import aliyun_face_detector
+from backend.extensions import app, db
+from backend import utils
+import logging
+
+def add_split_for_person(person_id):
+    person = models.Person.query.filter(models.Person.id == person_id).first()
+    new_person = models.Person(name=f'{person.name}_with_crop', user_id = person.user_id)
+    db.session.add(new_person)
+    db.session.commit()
+
+    sources = models.Source.query.filter(models.Source.person_id == person_id, models.Source.base_img_key != None).all()
+    
+    for source in sources:
+        logging.info(f'start to get img {source.base_img_key}')
+        img = utils.oss_get(source.base_img_key)
+        logging.info(f'finish to get img {source.base_img_key}')
+
+        crop_img = aliyun_face_detector.aliyun_human_segmentation(img)
+        if crop_img:
+            crop_img_key = f'{source.base_img_key[:-4]}_crop.jpg'
+            utils.oss_put(crop_img_key, crop_img)
+            new_source = models.Source(base_img_key=crop_img_key, user_id=source.user_id, type=source.type, person_id=new_person.id)
+            db.session.add(new_source)
+            db.session.commit()
+            logging.info(f'add crop img {crop_img_key} for source {source.id}')
+        
+        # new_source = models.Source(base_img_key=source.base_img_key, user_id=source.user_id, type=source.type, person_id=new_person.id)
+        # db.session.add(new_source)
+        # db.session.commit()
+
+        break
+
+# 开始执行main 函数
+if __name__ == '__main__':
+    app.app_context().push()
+
+    add_split_for_person(263)
