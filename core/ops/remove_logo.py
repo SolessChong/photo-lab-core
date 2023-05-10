343477fde5d3df1498eebe67716857f5cc740dd9
chong <solesschong@gmail.com>
2023-05-10 20:13:55 +0800

    [Enhancement] Add to remove_logo.

--- core/ops/remove_logo.py
+++ core/ops/remove_logo.py
@@ -10,16 +10,13 @@ import re
 import math
 import shutil
 from pathlib import Path
-from core import templates
 import webuiapi
-from core import face_mask
-from core.libs.openpose.body import Body
-from core import pose_detect
 import numpy as np
 import typing
+import argparse
 from backend import models
-from core.libs.openpose.util import draw_bodypose
-from core.resource_manager import ResourceMgr, ResourceType, oss2buf, str2oss, read_cv2img, read_PILimg, write_PILimg
+from backend.extensions import db, app
+from core.resource_manager import ResourceMgr, ResourceType, oss2buf, str2oss, read_cv2img, read_PILimg, write_PILimg, bucket
 
 
 # create API client with custom host, port
@@ -32,32 +29,32 @@ api.set_options(options)
 Remove logo on position
 1. left top; 2. right top; 3. left bottom; 4. right bottom; 5. middle top; 6. middle bottom
 """
-def remove_logo_from_image(img: Image, position: int, length: int=600, width: int=100) -> Image:
+def remove_logo_from_image(img: Image, location: int, length: int=600, width: int=100) -> Image:
     img_size = img.size
     # render_size = conf.LORA_ROI_RENDERING_SETTINGS['size']  # Replace with your desired render size
     render_size = (1800, 512)
 
     # crop image covering logo area, of size rendering_size
-    if position == 1:  # left top
+    if location == 1:  # left top
         partial_img = img.crop((0, 0, render_size[0], render_size[1]))
         logo_coords = (0, 0, length, width)
-    elif position == 2:  # right top
+    elif location == 2:  # right top
         partial_img = img.crop((img_size[0] - render_size[0], 0, img_size[0], render_size[1]))
         logo_coords = (render_size[0] - length, 0, render_size[0], width)
-    elif position == 3:  # left bottom
+    elif location == 3:  # left bottom
         partial_img = img.crop((0, img_size[1] - render_size[1], render_size[0], img_size[1]))
         logo_coords = (0, render_size[1] - width, length, render_size[1])
-    elif position == 4:  # right bottom
+    elif location == 4:  # right bottom
         partial_img = img.crop((img_size[0] - render_size[0], img_size[1] - render_size[1], img_size[0], img_size[1]))
         logo_coords = (render_size[0] - length, render_size[1] - width, render_size[0], render_size[1])
-    elif position == 5:  # middle top
+    elif location == 5:  # middle top
         partial_img = img.crop((img_size[0]//2 - render_size[0]//2, 0, img_size[0]//2 + render_size[0]//2, render_size[1]))
         logo_coords = (render_size[0]//2 - length//2, 0, render_size[0]//2 + length//2, width)
-    elif position == 6:  # middle bottom
+    elif location == 6:  # middle bottom
         partial_img = img.crop((img_size[0]//2 - render_size[0]//2, img_size[1] - render_size[1], img_size[0]//2 + render_size[0]//2, img_size[1]))
         logo_coords = (render_size[0]//2 - length//2, render_size[1] - width, render_size[0]//2 + length//2, render_size[1])
     else:
-        raise Exception('Invalid position')
+        raise Exception('Invalid location')
 
     # create mask of size rendering_size, background(0,0,0), with logo area color (255,255,255)
     logo_mask = Image.new("RGB", render_size, (0, 0, 0))
@@ -71,41 +68,64 @@ def remove_logo_from_image(img: Image, position: int, length: int=600, width: in
         mask_image=logo_mask,
         width=render_size[0],
         height=render_size[1],
-        mask_blur=50,
+        mask_blur=20,
         inpaint_full_res=0,
-        inpainting_fill=1,
+        inpainting_fill=0,
         inpaint_full_res_padding=20,
         denoising_strength=0.7,
         sampler_name="DPM++ SDE Karras"
     ).images[0]
 
     # paste inpainted image back to original image
-    if position == 1:  # left top
+    if location == 1:  # left top
         img.paste(inpaint_partial_img, (0, 0))
-    elif position == 2:  # right top
+    elif location == 2:  # right top
         img.paste(inpaint_partial_img, (img_size[0] - render_size[0], 0))
-    elif position == 3:  # left bottom
+    elif location == 3:  # left bottom
         img.paste(inpaint_partial_img, (0, img_size[1] - render_size[1]))
-    elif position == 4:  # right bottom
+    elif location == 4:  # right bottom
         img.paste(inpaint_partial_img, (img_size[0] - render_size[0], img_size[1] - render_size[1]))
-    elif position == 5:  # middle top
+    elif location == 5:  # middle top
         img.paste(inpaint_partial_img, (img_size[0]//2 - render_size[0]//2, 0))
-    elif position == 6:  # middle bottom
+    elif location == 6:  # middle bottom
         img.paste(inpaint_partial_img, (img_size[0]//2 - render_size[0]//2, img_size[1] - render_size[1]))
 
     return img
 
-def remove_scene_logo(scene_id):
+def remove_scene_logo(scene_id, location, length=None, width=None):
+    app.app_context().push()
     oss_url = ResourceMgr.get_resource_oss_url(ResourceType.BASE_IMG, scene_id)
-    img = read_PILimg(oss_url)
-    img_rst = remove_logo_from_image(img, 3, length=200, width=350)
+    # add _bck to the end of the url before file extension, split by .
+    backup_oss_url = '.'.join(oss_url.split('.')[:-1]) + '_bck.' + oss_url.split('.')[-1]
+    # copy file to backup url
+    if not bucket.object_exists(backup_oss_url):
+        bucket.copy_object(bucket.bucket_name, oss_url, backup_oss_url)
+
+    img = read_PILimg(backup_oss_url)
+    img_rst = remove_logo_from_image(img, location, length=length, width=width)
+    img_rst.save('test_remove_logo.png')
     write_PILimg(img_rst, oss_url)
+    db.session.close()
 
 
 if __name__ == "__main__":
     from backend.extensions import app, db
-    app.app_context().push()
-    scene = models.Scene.query.get(2885)
-    img = read_PILimg(ResourceMgr.get_resource_oss_url(ResourceType.BASE_IMG, scene.scene_id))
-    img_rst = remove_logo_from_image(img, 3, length=200, width=350)
-    img_rst.save('test_remove_logo.png')
+    
+    # take scene from argparser
+    argparser = argparse.ArgumentParser()
+    # read scenes, list of int
+    argparser.add_argument("--scene", type=int, nargs='+', required=True, help="scene id")
+    argparser.add_argument("--location", type=int, required=True, help="logo location, range from 1 to 6, 1: left top; 2: right top; 3: left bottom; 4: right bottom; 5: middle top; 6: middle bottom")
+    # read length and width from argparser
+    argparser.add_argument("--length", type=int, help="logo length", default=600)
+    argparser.add_argument("--width", type=int, help="logo width", default=100)
+
+    args = argparser.parse_args()
+    scene = args.scene
+    location = args.location
+
+    print(f"scene_id: {scene}, location: {location}, length: {args.length}, width: {args.width}")
+    for scene_id in scene:
+        remove_scene_logo(scene_id, location, length=args.length, width=args.width)
+
+    db.session.close()
--- core/tests/test_dataset_quality.py
+++ core/tests/test_dataset_quality.py
@@ -8,8 +8,23 @@ class TestDatasetQuality(unittest.TestCase):
         app.app_context().push()
         return super().setUp()
     
+    """
+    Example output:
+
+    report:
+
+    {'num_score': 0.48, 'background_variety': 0.7605350748697917, 'face_pose_variety': 1.6519066737248347, 'jpeg_compression': 0.6788361425408058, 'blurriness': 0.7557867511400702, 'lighting': 0.31394730673895943}
+    
+    comment:
+
+    You have not uploaded enough photos.:
+    - Upload at least 20 photos.
+    - Try to take photos in different settings and environments.
+    - Vary the settings and environments for your photos.
+    - Use different props or decorations to add variety to the background.
+    """
     def test_dataset_quality(self):
         # test analyze_person
-        person_id = 140
+        person_id = 299
         report, comment = dataset_quality.analyze_person(person_id)
         self.assertTrue(report is not None and comment is not None) 
