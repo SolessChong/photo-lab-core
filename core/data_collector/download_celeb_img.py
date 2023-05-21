f24045a5f28b0b28f553660a3be04bc8439a17e8
chong <solesschong@gmail.com>
2023-05-22 00:07:32 +0800

    .

--- core/data_collector/download_celeb_img.py
+++ core/data_collector/download_celeb_img.py
@@ -11,14 +11,12 @@ API_KEY = "50b4fa16370d400fadb079dd7389aaba"
 # Define the search query and parameters
 search_url = "https://api.bing.microsoft.com/v7.0/images/search"
 headers = {"Ocp-Apim-Subscription-Key": API_KEY}
+MAX_RESULTS_PER_REQUEST = 50
 
 def download_images(person_name, offset, count):
-    params = {"q": person_name, "count": count, "offset": offset}
-
-    # Make the API request
-    response = requests.get(search_url, headers=headers, params=params)
-    response.raise_for_status()
-    results = response.json()
+    # Calculate the number of requests needed
+    num_requests = count // MAX_RESULTS_PER_REQUEST
+    remaining_images = count % MAX_RESULTS_PER_REQUEST
 
     # Create the "data" directory if it doesn't exist
     os.makedirs("data", exist_ok=True)
@@ -46,19 +44,31 @@ def download_images(person_name, offset, count):
 
     # Download the images using multiple threads
     with ThreadPoolExecutor(max_workers=20) as executor:
-        futures = [
-            executor.submit(download_image, idx, image["contentUrl"], person_dir, offset)
-            for idx, image in enumerate(results["value"])
-        ]
+        for i in range(num_requests):
+            params = {"q": person_name, "count": MAX_RESULTS_PER_REQUEST, "offset": offset + i * MAX_RESULTS_PER_REQUEST}
+            response = requests.get(search_url, headers=headers, params=params)
+            response.raise_for_status()
+            results = response.json()
 
-        # Wait for all tasks to complete before moving on
-        wait(futures)
+            futures = [
+                executor.submit(download_image, idx, image["contentUrl"], person_dir, offset + i * MAX_RESULTS_PER_REQUEST)
+                for idx, image in enumerate(results["value"])
+            ]
 
-        for future in as_completed(futures):
-            try:
-                future.result()
-            except Exception as e:
-                print(f"Error occurred during download: {e}")
+            wait(futures)
+
+        if remaining_images > 0:
+            params = {"q": person_name, "count": remaining_images, "offset": offset + num_requests * MAX_RESULTS_PER_REQUEST}
+            response = requests.get(search_url, headers=headers, params=params)
+            response.raise_for_status()
+            results = response.json()
+
+            futures = [
+                executor.submit(download_image, idx, image["contentUrl"], person_dir, offset + num_requests * MAX_RESULTS_PER_REQUEST)
+                for idx, image in enumerate(results["value"])
+            ]
+
+            wait(futures)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Download images.')
